73643fa4300c209808763e5bc5d147c4daa5fff2
WoutVossen <vossen.w@hotmail.com>
2025-09-21 22:05:21 +0200

    Add new data

--- preprocessing/upload_hf.py
+++ preprocessing/upload_hf.py
@@ -1,4 +1,5 @@
 import os
+import time
 from huggingface_hub import login
 from dotenv import load_dotenv
 from huggingface_hub import HfApi, upload_file
@@ -9,12 +10,14 @@ login(os.environ.get("HF_TOKEN"))
 
 
 api = HfApi()
-# repo_url = api.create_repo(
-#    repo_id="pixel-art-finetune-dataset-1024-v2",
-##    repo_type="dataset",
-#   private=False,
-# )
-# print(repo_url)
+repo_url = api.create_repo(
+    repo_id="pixel-art-finetune-dataset-1024-v2",
+    repo_type="dataset",
+    private=False,
+)
+
+# wait for 10 seconds (until repo is created)
+time.sleep(10)
 upload_file(
     path_or_fileobj="data/cleaned_sprites_v2/dataset-1024.parquet",
     path_in_repo="data/dataset-1024.parquet",
